5ba3267317f7e7c0b96e4e387d26790e79318985
reltoom <ariel.toom@gmail.com>
2024-05-19 13:06:27 +0200

    Setup gunicorn and procfile

--- characterideas/settings.py
+++ characterideas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d816rlehe1k-gnx$*=vu33^f6451xox!&6p(8gd678$(qfllrj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
 
 
 # Application definition
