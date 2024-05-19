79972ca67007c3c6c11c95cde27fe0157dc71663
reltoom <ariel.toom@gmail.com>
2024-05-19 14:12:49 +0200

    Created the Character model in Idea
    connected it to admin so can be accessed in admin panel

--- idea/models.py
+++ idea/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Character(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    creator = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="idea_characters"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
