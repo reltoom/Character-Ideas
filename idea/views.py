52ce103e46604e68f1e26deba963a1a5b548b9aa
reltoom <ariel.toom@gmail.com>
2024-05-20 14:40:15 +0200

    Added that users can make a comment and get message for feedback about it

--- idea/views.py
+++ idea/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Character
+from .forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 class CharacterList(generic.ListView):
@@ -27,11 +29,24 @@ def character_detail(request, slug):
     character = get_object_or_404(queryset, slug=slug)
     comments = character.comments.all().order_by("-created_on")
     comment_count = character.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.creator = request.user
+            comment.character = character
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval')
+            
+    comment_form = CommentForm()
 
     return render(
         request,
         "idea/character_detail.html",
         {"character": character,
          "comments": comments,
-         "comment_count": comment_count,},
+         "comment_count": comment_count,
+         "comment_form": comment_form,},
     )
