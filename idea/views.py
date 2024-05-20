b37eb8d618d6d603a2f91fc93cc267494eb09530
reltoom <ariel.toom@gmail.com>
2024-05-20 09:28:09 +0200

    Added base and index html templates
    Added base code from walkthrough to get the layout

--- idea/views.py
+++ idea/views.py
@@ -5,3 +5,5 @@ from .models import Character
 # Create your views here.
 class CharacterList(generic.ListView):
     queryset = Character.objects.filter(status=1)
+    template_name = "idea/index.html"
+    paginate_by = 2
