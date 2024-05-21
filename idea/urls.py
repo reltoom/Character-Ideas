6c1bd9a77c67123e85401932b25b117f3c650193
reltoom <ariel.toom@gmail.com>
2024-05-21 08:44:17 +0200

    Added that user can delete their own comments
    They will get a pop up asking if they are sure they want to delete and then message that it has been deleted.

--- idea/urls.py
+++ idea/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<slug:slug>/', views.character_detail, name='character_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
--- idea/views.py
+++ idea/views.py
@@ -74,3 +74,20 @@ def comment_edit(request, slug, comment_id):
             messages.add_message(request, messages.ERROR, 'Error updating comment!')
 
     return HttpResponseRedirect(reverse('character_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Character.objects.filter(status=1)
+    character = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.creator == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('character_detail', args=[slug]))
