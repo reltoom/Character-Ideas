43c7a13f4d17fb938fccc0a1a24af3727212d311
reltoom <ariel.toom@gmail.com>
2024-05-21 09:02:27 +0200

    Added cloudinary to store images
    hooked up cloudinary as images storage location

--- idea/models.py
+++ idea/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -10,6 +11,7 @@ class Character(models.Model):
     creator = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="idea_characters"
     )
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
