cbeddaaeee7c881c132647b686e49b95a215de2a
reltoom <ariel.toom@gmail.com>
2024-05-22 16:00:00 +0200

    Adding css styling
    adding custom CSS to project

--- characterideas/settings.py
+++ characterideas/settings.py
@@ -21,13 +21,9 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
