06febce06510b2d0c8ef97c7cd8fe094369ebabf
reltoom <ariel.toom@gmail.com>
2024-05-22 13:59:09 +0200

    collected static so it will work on heroku

--- staticfiles/js/delete_character.js
+++ staticfiles/js/delete_character.js
@@ -2,15 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const deleteButtons = document.getElementsByClassName("delete-char-button");
     const deleteConfirm = document.getElementById("delete-char-confirm");
     const deleteModal = new bootstrap.Modal(document.getElementById("delete-char-modal"));
-    const modalBody = document.getElementById("delete-char-modal-body");
   
     for (let i = 0; i < deleteButtons.length; i++) {
-      deleteButtons[i].addEventListener("click", (e) => {
-        const characterSlug = e.target.getAttribute("data-character_slug");
-        const characterTitle = modalBody.getAttribute("data-character_title");
-        deleteConfirm.href = `${deleteConfirm.dataset.baseUrl}${characterSlug}`;
-        modalBody.innerText = `Are you sure you want to delete ${characterTitle}?`;
-        deleteModal.show();
-      });
+        deleteButtons[i].addEventListener("click", (e) => {
+            e.preventDefault(); // Preventing the default anchor behavior otherwise throws 404 error
+            const characterSlug = e.target.getAttribute("data-character-slug");
+            const characterTitle = e.target.closest('.card-body').querySelector('.card-title').innerText;
+            const characterTitlePlaceholder = document.getElementById("character-title-placeholder");
+            characterTitlePlaceholder.innerText = characterTitle;
+            // Ensure characterSlug is not undefined before setting deleteConfirm href
+            if (characterSlug) {
+                // Set the delete confirmation URL properly
+                deleteConfirm.href = `/${characterSlug}/delete/`;
+                deleteModal.show();
+            } else {
+                console.error("Character slug is undefined.");
+            }
+        });
     }
 });
